4c2cf9a310d18e36ec0d2f41fa92beb59919b828
shen_yangyang <shen_yangyang163@163.com>
2024-03-24 14:40:43 +0800

    first vue component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>To-Do List</h1>
+  <ul>
+    <li>
+      <demo-component></demo-component>
+      <demo-component label="My todo item"></demo-component>
+    </li>
+  </ul>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import DemoComponent from "./components/DemoComponent.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // 本地注册组件
+    HelloWorld,
+    DemoComponent
   }
 }
 </script>
---
+++ src/components/DemoComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" checked="false"/>
+    <label for="todo-item">{{ label }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {required: true, type: String, default: 'New item'},
+    done: {default: false, type: Boolean}
+  }
+};
+</script>
