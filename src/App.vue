793d8573059a9e05b8d76cc511a94a445126357f
shen_yangyang <shen_yangyang163@163.com>
2024-03-24 22:47:58 +0800

    to do item component

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,50 @@
   <HelloWorld msg="Welcome to Your Vue.js App"/>
   <h1>To-Do List</h1>
   <ul>
-    <li>
-      <demo-component label="My todo item" :done="true"></demo-component>
-      <demo-component label="My todo item" :done="false"></demo-component>
-      <demo-component label="My todo item" :done="false"></demo-component>
+    <li v-for="item in ToDoItems" :key="item.id">
+      <to-do-item :label="item.label" :done="true" :id="item.id"></to-do-item>
     </li>
   </ul>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import DemoComponent from "./components/DemoComponent.vue";
+// import DemoComponent from "./components/DemoComponent.vue";
+import ToDoItem from "./components/ToDoItem.vue";
+import uniqueId from "lodash.uniqueid";
 
 export default {
   name: 'App',
   components: {
     // 本地注册组件
     HelloWorld,
-    DemoComponent
+    ToDoItem,
+  },
+  data() {
+    return {
+      ToDoItems: [
+        {
+          id: uniqueId("todo-"),
+          label: "Learn Vue",
+          done: false
+        },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a Vue project with the CLI",
+          done: true,
+        },
+        {
+          id: uniqueId("todo-"),
+          label: "Have fun",
+          done: true
+        },
+        {
+          id: uniqueId("todo-"),
+          label: "Create a to-do list",
+          done: false
+        },
+      ],
+    }
   }
 }
 </script>
